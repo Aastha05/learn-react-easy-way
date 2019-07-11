c08c86dd079192acb339bf01027c1cd4e2b785c7
Aastha05 <aastha05@live.com>
2019-07-11 15:37:58 +0000

    header updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,46 @@
 /** @jsx jsx */
-import { jsx } from '@emotion/core';
+import { jsx, css } from '@emotion/core';
+
+const headerStyles = {
+    wrapper: {
+        display: 'flex',
+        flexDirection: 'row'
+    }
+}
 
 export default function Header() {
-  return <div css={getHeaderWrapperCSS()}>Hey It's header</div>;
+//   return <div css={getHeaderWrapperCSS()}>Hey It's header</div>;
+return (
+    <div css={getHeaderWrapperCSS()}>
+        <div className="brand-name">
+            <span>
+                __learning(.)int
+            </span>
+        </div>
+        <div className="navigation">
+            <ul>
+                <li>
+                    <a>Menu 1</a>
+                </li>
+                <li>
+                    <a>Menu 2</a>
+                </li>
+                <li>
+                    <a>Menu 3</a>
+                </li>
+                <li>
+                    <a>Menu 4</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+)
 }
 
 function getHeaderWrapperCSS() {
   return {
     display: 'flex',
-    flexDirection: 'row'
+    flexDirection: 'row',
+    // background: '#008080'
   };
 }
